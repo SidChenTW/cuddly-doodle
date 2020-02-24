0eaf1fcfb3f85c55e6f92b3c4e6339447cba01c5
SidChen <yingchih2009@gmail.com>
2020-02-25 00:56:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import os
 import sys
 
-def data_test():
+def AlwaysTrue():
+    print ("AlwaysTrue()", end=' ')
+    return True
+
+def AlwaysFalse():
+    print ("AlwaysFalse()", end=' ')
+    return False
+    
+
+def int_test():
     print (bin(50))
     print (hex(50))
     print (oct(50))
@@ -10,7 +19,40 @@ def data_test():
     
     for i in range (-5, 5):
         print (i , round(9012345.678901, i))
-        
     
+    a = 255
+    print (a.bit_length(), bin(a))
+    print ((-255).bit_length(), bin(-255))
+    
+def bool_test():
+    print ("AlwaysTrue() and AlwaysTrue()")
+    if (AlwaysTrue() and AlwaysTrue()):
+        pass
+    print ()
+    
+    print ("AlwaysFalse() and AlwaysTrue()")
+    if (AlwaysFalse() and AlwaysTrue()):
+        pass
+    print ()
+    
+    print ("AlwaysFalse() or AlwaysFalse()")
+    if (AlwaysFalse() or AlwaysFalse()):
+        pass
+    print ()
+    
+    print ("AlwaysTrue() or AlwaysFalse()")
+    if (AlwaysTrue() or AlwaysFalse()):
+        pass
+    print ()
+
+def equal_float(a, b):
+    return abs(a-b) <= sys.float_info.epsilon
+
+def float_test():
+    print ([0.0, 5.4, -2.5])
+    print (sys.float_info.epsilon)
+
 if __name__ == '__main__':
-    data_test()
+    int_test()
+    bool_test()
+    float_test()
